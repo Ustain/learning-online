25f7c889467e3ddf11e6d7908d275b8ae27d2a43
dshyjtdes8888 <128817774+dshyjtdes8888@users.noreply.github.com>
2024-04-25 19:14:42 +0800

    Update initrouter.go

--- initrouter.go
+++ initrouter.go
@@ -9,7 +9,7 @@ import (
 // 参数r是一个gin.Engine实例，用于构建路由。
 func InitRouter(r *gin.Engine) {
 
-	// 创建一个名为/media_api/ningmeng的路由组，用于将相关的API分组。
+	// 创建一个名为/media的路由组，用于将相关的API分组。
 	apiRouter := r.Group("/media")
 
 	apiRouter.POST("/files/", media_api.ListMediaFiles)               // 获取媒资数据的API
